b5e86c2382762c97cbc95ef06aadebf8befac315
Ivan Statelov <statelov.ivan@yahoo.com>
2023-04-10 11:23:23 +0300

    Add Tasks for Lab
    Problem Solutions for Lab - Regular Expressions

---
+++ Regular Expressions - Lab/2. Match Phone Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2._Match_Phone_Number1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\+359 2 [0-9]{3} [0-9]{4}|\+359-2-[0-9]{3}-[0-9]{4}\b";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
