765236828b055c695afa78b8b928357c1bc4c6a5
Ivan Statelov <statelov.ivan@yahoo.com>
2023-09-24 17:53:21 +0300

    Add Tasks for More Exercises Lists
    Problem Solutions for More Exercises: Lists

---
+++ More Exercises Lists/Messaging/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();
+
+            string textInput = Console.ReadLine();
+
+            string textOutput = string.Empty;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                string currentElement = numbers[i].ToString();
+
+                numbers[i] = 0;
+
+                numbers = CalculateTheSumOfDigitsForEachElement(numbers, currentElement,i);
+            }
+
+            int index = 0;
+
+            for (int k = 0; k < numbers.Count; k++)
+            {
+                if (numbers[k] > textInput.Length)
+                {
+                    numbers[k] = numbers[k] % textInput.Length;    
+                }
+                else if (numbers[k] == textInput.Length)
+                {
+                    index = 0;
+                }
+
+                index = numbers[k];
+
+                textOutput += textInput[index];
+
+                textInput = RemoveTheTakenChar(textInput, index);
+            }
+            Console.WriteLine(textOutput);
+        }
+
+        static List<int> CalculateTheSumOfDigitsForEachElement(List<int> numbers, string currentElement, int i)
+        {
+            for (int j = 0; j < currentElement.Length; j++)
+            {
+                string str = currentElement[j].ToString();
+
+                numbers[i] += int.Parse(str);
+            }
+            return numbers;
+        }
+
+        static string RemoveTheTakenChar(string textInput, int index)
+        {
+            string currentText = String.Empty;
+
+            for (int i = 0; i < textInput.Length; i++)
+            {
+                if (i == index)
+                {
+                    continue;
+                }
+                currentText += textInput[i];
+            }
+            return currentText;
+        }
+    }
+}
---
+++ More Exercises Lists/Take Or Skip Rope/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Take_Or_Skip_Rope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+
+            List<int> numbersList = new List<int>();
+            List<string> nonNumbersList = new List<string>();
+
+            foreach (var ch in encryptedMessage)
+            {
+                if (char.IsDigit(ch))
+                {
+                    int currDigit = int.Parse(ch.ToString());
+                    numbersList.Add(currDigit);
+                }
+                else
+                {
+                    nonNumbersList.Add(ch.ToString());
+                }
+            }
+
+            List<int> takeList = new List<int>();
+            List<int> skipList = new List<int>();
+
+            for (int i = 0; i < numbersList.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    takeList.Add(numbersList[i]);
+                }
+                else
+                {
+                    skipList.Add(numbersList[i]);
+                }
+            }
+
+            string decryptedMessage = string.Empty;
+
+            for (int j = 0; j < skipList.Count; j++)
+            {
+                int skipElements = skipList[j];
+                int takeElements = takeList[j];
+
+
+                if (takeElements > nonNumbersList.Count)
+                {
+                    takeElements = nonNumbersList.Count;
+                }
+
+                int counter = takeElements;
+
+                for (int i = 0; i < nonNumbersList.Count; i++)
+                {
+                    if (counter == 0)
+                    {
+                        break;
+                    }
+                    counter--;
+
+                    decryptedMessage += nonNumbersList[i];
+                }
+
+                nonNumbersList.RemoveRange(0, takeElements);
+
+                if (skipElements > nonNumbersList.Count)
+                {
+                    skipElements = nonNumbersList.Count;
+                }
+
+                nonNumbersList.RemoveRange(0, skipElements);
+                
+
+            }
+            Console.WriteLine(decryptedMessage);
+
+        }
+ 
+    }
+ 
+}
